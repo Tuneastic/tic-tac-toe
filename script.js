0c27f8f0e52f3e135647faeb86afc5425e082f81
Tuneastic <214825290+Tuneastic@users.noreply.github.com>
2025-11-26 14:05:51 +0100

    Fiddle with logic for the gameController module

--- script.js
+++ script.js
@@ -1,58 +1,67 @@
-//logic for controlling the gameboard state
 const Gameboard = (() => {
-
     const board = ["", "", "", "", "", "", "", "", ""];
-
     const resetBoard = function() {
+        
         for(let i=0; i<8; i++){
             board[i]='';
         }
     };
-
     const placeMark = function(index, mark) {
         if (board[index] === '') {
             board[index] = mark;
         }
     };
-    
     const getBoard = () => board;
-
     return {resetBoard,placeMark,getBoard};
-    
 })();
-
-//logic for controlling the game state
 const GameController = (() => {
-    
-    const Player = (name, mark) => ({ name, mark });
     let player1, player2;
     let currentPlayer;
-
+    const Player = (name, mark) => ({ name, mark });
+    const changePlayer = () => {currentPlayer = currentPlayer === player1 ? player2 : player1};
     const startGame = (name1, name2) => {
         Gameboard.resetBoard();
         player1 = Player(name1, 'X');
         player2 = Player(name2, 'O');
         currentPlayer = player1;
     };
-
-    const changePlayer = () => {
-        currentPlayer = currentPlayer === player1 ? player2 : player1;
-    };
-
+    const finishGame = (player) => {
+        alert(`${player.name} wins! Congratulations! ${player.mark} rules the day :D`);
+        Gameboard.resetBoard();
+        player1 = null;
+        player2 = null;
+        currentPlayer = null;
+    }
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
     const checkWinConditions = (currentPlayer) => {
+        const board = Gameboard.getBoard();
         if (currentPlayer === player1) {
-            
+            for (const combination of winConditions) {
+                if (combination.every(index => board[index] === 'X')) {
+                    finishGame(player1);
+                    break;
+                }
+            }
         }
-
         else if (currentPlayer === player2) {
-
+            for (const combination of winConditions) {
+                if (combination.every(index => board[index] === 'O')) {
+                    finishGame(player2);
+                    break;
+                }
+            }
         }
     };
-
     const playTurn = () => {};
-
     return { startGame, playTurn};
 })();
-
-//logic for controlling the screen render state
 const DisplayController = (() => {})();
